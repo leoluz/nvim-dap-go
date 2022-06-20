f98f04d87d21336b5f7cf8ee9d23dbf045b9c025
xuxin <xuxinmight@gmail.com>
2022-06-20 11:45:27 +0800

    configure stderr stream output

--- lua/dap-go.lua
+++ lua/dap-go.lua
@@ -32,18 +32,20 @@ end
 local function setup_go_adapter(dap)
   dap.adapters.go = function(callback, config)
     local stdout = vim.loop.new_pipe(false)
+    local stderr = vim.loop.new_pipe(false)
     local handle
     local pid_or_err
     local host = config.host or "127.0.0.1"
     local port = config.port or "38697"
     local addr = string.format("%s:%s", host, port)
     local opts = {
-      stdio = {nil, stdout},
+      stdio = {nil, stdout, stderr},
       args = {"dap", "-l", addr},
       detached = true
     }
     handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
       stdout:close()
+      stderr:close()
       handle:close()
       if code ~= 0 then
         print('dlv exited with code', code)
@@ -51,6 +53,14 @@ local function setup_go_adapter(dap)
     end)
     assert(handle, 'Error running dlv: ' .. tostring(pid_or_err))
     stdout:read_start(function(err, chunk)
+      assert(not err, err)
+      if chunk then
+        vim.schedule(function()
+          require('dap.repl').append(chunk)
+        end)
+      end
+    end)
+    stderr:read_start(function(err, chunk)
       assert(not err, err)
       if chunk then
         vim.schedule(function()
