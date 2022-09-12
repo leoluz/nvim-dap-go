4aa769685690fd9219e6813994f40209d9d58020
Freddie Haddad <freddie.haddad@outlook.com>
2022-09-12 13:51:22 -0700

    Add option to debug with command-line arguments.
    This commit introduces a new option called:
    
       Debug (Arguments)
    
    When selected, the user will be prompted to enter
    any command line arguments to provide to the program
    being debugged.
    
    The initial implementation is naive in that it splits
    the input string using the space character (" ") as
    the separator.

--- lua/dap-go.lua
+++ lua/dap-go.lua
@@ -29,6 +29,25 @@ local function load_module(module_name)
   return module
 end
 
+local function get_arguments()
+  local co = coroutine.running()
+  if co then
+    return coroutine.create(function()
+      local args = {}
+      vim.ui.input({ prompt = 'Args: ' }, function(input)
+        args = vim.split(input or "", " ")
+      end)
+      coroutine.resume(co, args)
+    end)
+  else
+    local args = {}
+    vim.ui.input({ prompt = 'Args: ' }, function(input)
+      args = vim.split(input or "", " ")
+    end)
+    return args
+  end
+end
+
 local function setup_go_adapter(dap)
   dap.adapters.go = function(callback, config)
     local stdout = vim.loop.new_pipe(false)
@@ -74,6 +93,13 @@ local function setup_go_configuration(dap)
       name = "Debug",
       request = "launch",
       program = "${file}",
+    },
+    {
+      type = "go",
+      name = "Debug (Arguments)",
+      request = "launch",
+      program = "${file}",
+      args = get_arguments,
     },
     {
       type = "go",
