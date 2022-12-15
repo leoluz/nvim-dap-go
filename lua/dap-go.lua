bd9823da22165ea4aa7ec232e8c3a9b3158e017a
Leonardo Luz Almeida <leoluz@users.noreply.github.com>
2022-12-15 14:21:51 -0500

    Use adapter config provided by nvim-dap (#26)
    * Use adapter config provided by nvim-dap
    
    * Fix format issues
    
    * One more format fix
    
    * More formating

--- lua/dap-go.lua
+++ lua/dap-go.lua
@@ -51,63 +51,15 @@ local function get_arguments()
   end
 end
 
-local function setup_go_adapter(dap)
-  dap.adapters.go = function(callback, config)
-    local stdout = vim.loop.new_pipe(false)
-    local stderr = vim.loop.new_pipe(false)
-    local handle
-    local pid_or_err
-    local host = config.host or "127.0.0.1"
-    local port = config.port or "38697"
-    local addr = string.format("%s:%s", host, port)
-
-    if config.request == "attach" and config.mode == "remote" then
-      -- Not starting delve server automatically in "Attach remote."
-      -- Will connect to delve server that is listening to [host]:[port] instead.
-      -- Users can use this with delve headless mode:
-      --
-      -- dlv debug -l 127.0.0.1:38697 --headless ./cmd/main.go
-      --
-      local msg = string.format("connecting to server at '%s'...", addr)
-      print(msg)
-    else
-      local opts = {
-        stdio = { nil, stdout, stderr },
-        args = { "dap", "-l", addr },
-        detached = true,
-      }
-      handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
-        stdout:close()
-        stderr:close()
-        handle:close()
-        if code ~= 0 then
-          print("dlv exited with code", code)
-        end
-      end)
-      assert(handle, "Error running dlv: " .. tostring(pid_or_err))
-      stdout:read_start(function(err, chunk)
-        assert(not err, err)
-        if chunk then
-          vim.schedule(function()
-            require("dap.repl").append(chunk)
-          end)
-        end
-      end)
-      stderr:read_start(function(err, chunk)
-        assert(not err, err)
-        if chunk then
-          vim.schedule(function()
-            require("dap.repl").append(chunk)
-          end)
-        end
-      end)
-    end
-
-    -- Wait for delve to start
-    vim.defer_fn(function()
-      callback({ type = "server", host = host, port = port })
-    end, 100)
-  end
+local function setup_delve_adapter(dap)
+  dap.adapters.go = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      command = "dlv",
+      args = { "dap", "-l", "127.0.0.1:${port}" },
+    },
+  }
 end
 
 local function setup_go_configuration(dap, configs)
@@ -163,12 +115,11 @@ local function setup_go_configuration(dap, configs)
       table.insert(dap.configurations.go, config)
     end
   end
-
 end
 
 function M.setup(configs)
   local dap = load_module("dap")
-  setup_go_adapter(dap)
+  setup_delve_adapter(dap)
   setup_go_configuration(dap, configs)
 end
 
